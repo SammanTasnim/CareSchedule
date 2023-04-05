b0ea5fc705b63c48d970d77143d748db5d55636e
Sagor <sagorsutra16@gmail.com>
2023-04-06 03:55:33 +0600

    second changes

--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -26,6 +26,23 @@ const Signup = (props) => {
     
       </View>
 
+
+      <View>
+        <Text style={{ fontSize: 40 }}>Hello World</Text>
+        <Button
+          title="Go To Another Page"
+          onPress={() => {
+            props.navigation.navigate("List");
+          }}
+        />
+        <Button
+          title="Go To Counter Page"
+          onPress={() => {
+            props.navigation.navigate("Counter");
+          }}
+        />
+      </View> 
+
       { <View>
         <Text style={{ fontSize: 40 }}>Hello World</Text>
         <Button
@@ -40,7 +57,8 @@ const Signup = (props) => {
             props.navigation.navigate("Counter");
           }}
         />
-      </View> }
+      </View> 
+      }
     </SafeAreaView>
   );
 };
